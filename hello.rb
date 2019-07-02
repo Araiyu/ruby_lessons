5e5cb0b48910d00852cf381668463b8c815079de
新井　雄介 <ay088055@gmail.com>
2019-07-02 21:01:02 +0900

    F-16

--- hello.rb
+++ hello.rb
@@ -1,17 +1,25 @@
-#while
-# i = 0
+#for
 
-# while i < 10 do
-
-# 	puts "#{i}:hello"
-# 	i += 1
+# for i in 15..20 do
+# 	p i
 # end
 
-#times 回数決まっている場合便利！！
-
-# 10.times do |i|
-# 	puts "#{i}: hello"
+# for color in ["red", "blue"] do
+# 	p color
 # end
 
+# for name, score in {taguchi:200, fkoji:400 } do
+# 	puts "#{name}: #{score}"
+# end
 
-10.times {|i| puts "#{i}: hello"}
+(15..20).each do |i|
+	p i
+end
+
+["red", "blue"].each do |color|
+	p color
+end
+
+{taguchi:200, fkoji:400}.each do |name,score|
+	puts "#{name}: #{score}"
+end
